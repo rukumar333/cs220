f5c28d6e3b545b80ef9a595b136562469bd63430
Rushil <rukumar333@gmail.com>
2016-03-18 22:56:13 -0400

    edited hw

--- hw/6/main.c
+++ hw/6/main.c
@@ -7,12 +7,12 @@ void func(int x){
 }
 
 int main(){
-    /* enum strikers { Giroux, Bony, Rooney, Hazard, Aguero, Costa, Kane } player; */
-    /* enum teams { Arsenal, ManCity, ManU, Chelsea, Tottenham } team; */
+    enum strikers { Giroux, Bony, Rooney, Hazard, Aguero, Costa, Kane } player;
+    enum teams { Arsenal, ManCity, ManU, Chelsea, Tottenham } team;
     /* team = Arsenal; */
     /* player = Rooney; */
-    /* if (player==Giroux) team=Arsenal; */
-    /* else if (player==Bony) team=ManCity; */
-    /* else if (player==Rooney) team=ManU; */
+    if (player==Giroux) team=Arsenal;
+    else if (player==Bony) team=ManCity;
+    else if (player==Rooney) team=ManU;
     return 1;
 }
