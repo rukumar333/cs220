59e8ff7897205c14770357207d93b362abb39948
Rushil Kumar <rkumar6@cs.binghamton.edu>
2016-05-03 17:37:29 -0400

    setting registers but still segfaulting

--- projects/proj4/hack.s
+++ projects/proj4/hack.s
@@ -5,54 +5,48 @@
 alterGrades:
 .LFB0:
 	.cfi_startproc
-	pushl	%ebp
+
 	.cfi_def_cfa_offset 8
 	.cfi_offset 5, -8
-	movl	%esp, %ebp
-	movl	$0xffffdab8, (%ebp)
 	.cfi_def_cfa_register 5
-	subl	$16, %esp
-	movl	8(%ebp), %eax
+	movl	$0x0804c8c0, %eax
 	movl	$90, 64(%eax)
-	movl	8(%ebp), %eax
+	movl	$0x0804c8c0, %eax
 	addl	$400, %eax
 	movl	$80, 64(%eax)
-	movl	8(%ebp), %eax
+	movl	$0x0804c8c0, %eax
 	addl	$800, %eax
 	movl	$90, 64(%eax)
-	movl	8(%ebp), %eax
+	movl	$0x0804c8c0, %eax
 	addl	$1200, %eax
 	movl	$100, 64(%eax)
-	movl	8(%ebp), %eax
+	movl	$0x0804c8c0, %eax
 	addl	$1600, %eax
 	movl	$9, 64(%eax)
-	movl	8(%ebp), %eax
+	movl	$0x0804c8c0, %eax
 	addl	$2000, %eax
 	movl	$9, 64(%eax)
-	movl	8(%ebp), %eax
+	movl	$0x0804c8c0, %eax
 	addl	$2400, %eax
 	movl	$9, 64(%eax)
 	movl	$7, -4(%ebp)
 	movl	-4(%ebp), %eax
 	imull	$400, %eax, %edx
-	movl	8(%ebp), %eax
+	movl	$0x0804c8c0, %eax
 	addl	%edx, %eax
 	movl	$9, 64(%eax)
-	movl	8(%ebp), %eax
+	movl	$0x0804c8c0, %eax
 	addl	$3200, %eax
 	movl	$95, 64(%eax)
-	movl	8(%ebp), %eax
+	movl	$0x0804c8c0, %eax
 	addl	$3600, %eax
 	movl	$95, 64(%eax)
-	movl	8(%ebp), %eax
+	movl	$0x0804c8c0, %eax
 	addl	$4000, %eax
 	movl	$93, 64(%eax)
-	leave
 	.cfi_restore 5
 	.cfi_def_cfa 4, 4
-	movl	$0x5570aff4, %ebx
-	addl	$-0x4, %esp
-	movl	$0x08048792, (%esp)
+	pushl	$0x08048792
 	ret
 	.cfi_endproc
 .LFE0:
