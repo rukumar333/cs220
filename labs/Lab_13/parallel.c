ce34412fd1a5430a04477a666738d9267cc33c1a
Rushil Kumar <rkumar6@cs.binghamton.edu>
2016-05-06 15:05:30 -0400

    finished lab 13

--- labs/Lab_13/parallel.c
+++ labs/Lab_13/parallel.c
@@ -5,10 +5,50 @@
 #include <stdbool.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#define MAXKIDS 10
+int childpid[MAXKIDS];
 
 int main(int argc,char **argv) {
-
-	printf("Not implmented yet\n");
-	exit(1);
+  int ai = 1;
+  pid_t pid = -1;
+  int i = 0;
+  while(i < MAXKIDS && ai < argc){    
+    /* childpid[i] = fork(); */
+    pid = fork();
+    childpid[i] = pid;
+    if(pid == 0){
+      char * srch;
+      char * file;
+      srch=argv[ai+1];
+      file=argv[ai];
+      int hits = numHits(file, srch);
+      printf("File %s has %d instances of %s\n",file,hits,srch);
+      exit(0);
+    }
+    ai+=2;
+    i ++;
+  }
+  int cstat;
+  int k = 0;
+  while(k < i){
+    waitpid(childpid[k], &cstat, NULL);
+    printf("Child status: %d\n", cstat);
+    k ++;
+  }
+  exit(0);
+  /* int ai=1; */
+  /* while (ai<argc) { */
+    
+  /*   char * srch; */
+  /*   char * file; */
+  /*   srch=argv[ai+1]; */
+  /*   file=argv[ai]; */
+  /*   pid_t pid = fork(); */
+		
+  /*   int hits=numHits(file,srch); */
+  /*   printf("File %s has %d instances of %s\n",file,hits,srch); */
+  /*   ai+=2; */
+  /* } */
+  /* exit(0); */
 
 }
